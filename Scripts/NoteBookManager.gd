6617ebd902940bd8714f1c14e2bbc50836a02bbe
Anshu Chaurasiya <lazyanshu123@gmail.com>
2026-08-07 16:31:14 +0530

    Added a NoteBookManager

---
+++ Scripts/NoteBookManager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+static var Notes : Dictionary = {
+	"Security": {
+		"Title": "If you are reading this I am dead!",
+		"Body": "The gate's Password is 2050",
+	},
+	"Subject-01": {
+		"Title": "A complete Failure!",
+		"Body": "He Killed Everyone =D"
+	}
+}
+
+
+static func get_note(note_name):
+	return Notes[note_name]
+	
